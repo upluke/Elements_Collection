33ecd775ff82bb11180e5dd0794bf3b94cabd068
upluke <lukeforadream@gmail.com>
2021-09-12 11:01:28 -0700

    h-index

--- sorting/computing_the_h-index.py
+++ sorting/computing_the_h-index.py
@@ -1,1 +1,10 @@
-print("hello")
+def h_index(citations):
+    citations.sort()
+    size = len(citations)
+    for count, item in enumerate(citations):
+        if item >= size-count:
+            return size-count
+    return 0
+
+
+print(h_index([1, 4, 1, 4, 2, 1, 3, 5, 6]))  # 4
