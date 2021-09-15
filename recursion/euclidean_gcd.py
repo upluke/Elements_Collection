c3772bbdf8b33e9bb05f1421bed6121acdb6b85a
upluke <lukeforadream@gmail.com>
2021-09-14 22:58:45 -0700

    euclidean_gcd

---
+++ recursion/euclidean_gcd.py
@@ -0,0 +1,14 @@
+# 15.0
+
+# init：
+# def GCD(a, b):
+#   if b==0:
+#     return a
+#   return GCD(b,a%b)
+# print(GCD(270,192))
+
+def gcd(x, y):
+    return x if y == 0 else gcd(y, x % y)
+
+
+print(gcd(270, 192))
