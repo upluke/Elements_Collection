c0c798a95aba95bbdf2a49b5e7585c9726bd5f34
upluke <lukeforadream@gmail.com>
2021-09-17 23:11:31 -0700

    making_change

---
+++ greedy-algorithms-and-invariants/making_change.py
@@ -0,0 +1,13 @@
+# 0 % ANY NUMBER GREATER THAN 0 IS 0
+# we perform 6 iterations, and each iteration does a constant amount of computation,
+#  so the time complexity is O(1)
+def change_making(cents):
+    coins = [100, 50, 25, 10, 5, 1]
+    num_coins = 0
+    for coin in coins:
+        num_coins += cents//coin
+        cents %= coin
+    return num_coins
+
+
+print(change_making(90))
