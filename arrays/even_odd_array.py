1b511455e8b788d1dab0a1f19278f61d1f41a8d0
upluke <lukeforadream@gmail.com>
2021-10-02 23:30:44 -0700

    move all negative elements to end

--- arrays/even_odd_array.py
+++ arrays/even_odd_array.py
@@ -1,3 +1,30 @@
+
+
+# time:O(n)
+# space:O(1)
+
+from numpy import random
+
+
+def even_odd(A):
+
+    next_even, next_odd = 0, len(A) - 1
+    while next_even < next_odd:
+        if A[next_even] % 2 == 0:
+            next_even += 1
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            next_odd -= 1
+    return A
+
+
+nums = random.randint(10, size=(8))
+
+print("---", nums)
+
+print(even_odd(nums))
+
+
 # def even_odd(A):
 #   # two pointers
 #     l,r=0,len(A)-1
@@ -25,27 +52,32 @@
 
 # print(even_odd(nums))
 
+# 437 Move all negative elements to end (similar problem )
+# Input :
+# N = 8
+# arr[] = {1, -1, 3, 2, -7, -5, 11, 6 }
+# Output :
+# 1  3  2  11  6  -1  -7  -5
 
-# time:O(n)
-# space:O(1)
-
-from numpy import random
-
-
-def even_odd(A):
-
-    next_even, next_odd = 0, len(A) - 1
-    while next_even < next_odd:
-        if A[next_even] % 2 == 0:
-            next_even += 1
-        else:
-            A[next_even], A[next_odd] = A[next_odd], A[next_even]
-            next_odd -= 1
-    return A
+# Input :
+# N=8
+# arr[] = {-5, 7, -3, -4, 9, 10, -1, 11}
+# Output :
+# 7  9  10  11  -5  -3  -4  -1
 
 
-nums = random.randint(10, size=(8))
+# gfg:
 
-print("---", nums)
+def move(arr, n):
+    j = 0
+    for i in range(n):
+        if arr[i] >= 0:
+            arr[i], arr[j] = arr[j], arr[i]
+            j += 1
+    return arr
 
-print(even_odd(nums))
+
+# Driver program
+arr = [1, -1, -3, -2, 7, 5, 11, 6]
+n = len(arr)
+print(move(arr, n))
