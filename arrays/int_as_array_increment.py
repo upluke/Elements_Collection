ffc6ca56f84ba127c674e5286c59091ad4a8f8e7
upluke <lukeforadream@gmail.com>
2021-10-20 23:12:20 -0700

    plus one

---
+++ arrays/int_as_array_increment.py
@@ -0,0 +1,14 @@
+
+# Yt:
+def plusOne(digits):
+    for i in range(len(digits)-1, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        else:
+            digits[i] = 0
+    digits.insert(0, 1)
+    return digits
+
+
+print(plusOne([9, 5, 9]))
