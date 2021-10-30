929b8a4a61c3b7515cb5cc31c1a695f3ca7d5b13
upluke <lukeforadream@gmail.com>
2021-10-30 09:39:29 -0700

    h index

--- sorting/h-index.py
+++ sorting/h-index.py
@@ -1,13 +1,25 @@
-# 13.3
-# t: O(n log n)
-# s: O(1)
+# 13.3 computing the h-Index
+
+# https://leetcode.com/problems/h-index/
+
+# Example 1:
+# Input: citations = [3,0,6,1,5]
+# Output: 3
+# Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+# Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+
+# Example 2:
+# Input: citations = [1,3,1]
+# Output: 1
+
 def h_index(citations):
     citations.sort()
-    size = len(citations)
-    for count, item in enumerate(citations):
-        if item >= size-count:
-            return size-count
+    n = len(citations)
+
+    for i, c in enumerate(citations):
+        if c >= n-i:
+            return n-i
     return 0
 
 
-print(h_index([1, 4, 1, 4, 2, 1, 3, 5, 6]))  # 4
+print(h_index([3, 0, 6, 1, 5]))
