0a9cfcee5b9c8bec22dd9cd2fb0e536e91013a43
upluke <lukeforadream@gmail.com>
2021-12-11 16:40:36 -0800

    9.4 update

--- binary-trees/lowest_common_ancestor_with_parent.py
+++ binary-trees/lowest_common_ancestor_with_parent.py
@@ -1,19 +1,96 @@
 
 # 9.4 Compute the LCA when nodes have parent pointers
+# The time and space complexity are that of computing the depth, namely O(h) and O(1), respectively.
 # Lowest Common Ancestor of a Binary Tree III - LeetCode
 #
 
+# https://towardsdatascience.com/binary-tree-and-lowest-common-ancestor-58eddd433ac
+
 # ele:
-class BinaryTreeNode:
-    def __init__(self, data, left=None, right=None, parent=None):
-        self.data = data
+from collections import deque
+
+
+class Node:
+    def __init__(self, value, parent=None, left=None, right=None):
+        self.value = value
+        self.parent = parent
         self.left = left
         self.right = right
-        self.parent = parent
+
+    def add_to_dict(self, node_dict):
+
+        node_dict[self.value] = self
 
 
-def lca(node0,
-        node1):
+def create_bt(value_queue):
+    """create binary tree"""
+
+    if len(value_queue) <= 0:
+        return None, None
+
+    node_dict = {}
+
+    root = Node(value_queue.popleft())
+
+    root.add_to_dict(node_dict)
+
+    current_queue = deque()
+
+    current_queue.append(root)
+
+    while len(current_queue) > 0 and len(value_queue) > 0:
+
+        current_node = current_queue.popleft()
+
+        left = value_queue.popleft()
+        if left is not None:
+            current_node.left = Node(left, parent=current_node)
+            current_node.left.add_to_dict(node_dict)
+            current_queue.append(current_node.left)
+
+        right = value_queue.popleft()
+        if right is not None:
+            current_node.right = Node(right, parent=current_node)
+            current_node.right.add_to_dict(node_dict)
+            current_queue.append(current_node.right)
+
+    return root, node_dict
+
+
+def create_bt_fls(value_list):
+    """create binary create from list"""
+
+    return create_bt(deque(value_list))
+
+
+def lca(node1, node2):
+
+    parents = set()
+
+    cur_parent = node1
+    while cur_parent is not None:
+        parents.add(cur_parent)
+        cur_parent = cur_parent.parent
+
+    cur_parent = node2
+    while cur_parent is not None:
+        if cur_parent in parents:
+            return cur_parent
+        cur_parent = cur_parent.parent
+
+    return None
+
+
+ls1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, None, None, None, None, 9, 10]
+bt1, node_dict = create_bt_fls(ls1)
+lca1 = lca(node_dict[9], node_dict[10])
+print(lca1.value)
+
+# ele:
+
+
+def lca2(node0,
+         node1):
     def get_depth(node):
         depth = 0
         while node.parent:
@@ -38,8 +115,12 @@ def lca(node0,
     return node0
 
 
+lca2 = lca2(node_dict[9], node_dict[10])
+print(lca2.value)
+
 # ol:
 # for more solutions: https://nicksma.medium.com/lowest-common-ancestors-and-variations-493588b699f3
+
 
 class BinaryTreeNode:
     def __init__(self, data, left=None, right=None, parent=None):
@@ -49,8 +130,8 @@ class BinaryTreeNode:
         self.parent = parent
 
 
-def find_lca(n1: BinaryTreeNode, n2: BinaryTreeNode) -> BinaryTreeNode:
-    def get_depth(node: BinaryTreeNode) -> int:
+def find_lca(n1, n2):
+    def get_depth(node):
         depth = 0
         while node and node.parent:
             node = node.parent
