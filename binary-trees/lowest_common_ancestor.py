be4293f24ee2c2a462026a9dbc4655b10465aa0b
upluke <lukeforadream@gmail.com>
2021-12-11 16:39:13 -0800

    9.3 update

--- binary-trees/lowest_common_ancestor.py
+++ binary-trees/lowest_common_ancestor.py
@@ -28,73 +28,132 @@
 # O(n) solution to find LCS of two given values n1 and n2
 
 # A binary tree node
-import functools
-import collections
+# class newNode:
+#     # Constructor to create a new binary node
+#     def __init__(self, data):
+#         self.data =  data
+#         self.left = None
+#         self.right = None
+
+# # This function returns pointer to LCA of two given
+# # values n1 and n2
+# # This function assumes that n1 and n2 are present in
+# # Binary Tree
+# def findLCA(root, n1, n2):
+
+#     # Base Case
+#     if root is None:
+#         return None
+
+#     # If either n1 or n2 matches with root's data, report
+#     #  the presence by returning root (Note that if a data is
+#     #  ancestor of other, then the ancestor key becomes LCA
+#     if root.data == n1 or root.data == n2:
+#         return root
+
+#     # Look for datas in left and right subtrees
+#     left_lca = findLCA(root.left, n1, n2)
+#     right_lca = findLCA(root.right, n1, n2)
+
+#     # If both of the above calls return Non-NULL, then one data
+#     # is present in once subtree and other is present in other,
+#     # So this node is the LCA
+#     if left_lca and right_lca:
+#         return root
+
+#     # Otherwise check if left subtree or right subtree is LCA
+#     return left_lca if left_lca is not None else right_lca
+
+# root=newNode(3)
+# root.left=newNode(5)
+# root.right=newNode(1)
+# root.left.left=newNode(6)
+# root.left.right=newNode(2)
+# root.right.left=newNode(0)
+# root.right.right=newNode(8)
+# root.left.right.left=newNode(7)
+# root.left.right.right=newNode(4)
 
 
-class newNode:
-    # Constructor to create a new binary node
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-# This function returns pointer to LCA of two given
-# values n1 and n2
-# This function assumes that n1 and n2 are present in
-# Binary Tree
-
-
-def findLCA(root, n1, n2):
-
-    # Base Case
-    if root is None:
-        return None
-
-    # If either n1 or n2 matches with root's data, report
-    #  the presence by returning root (Note that if a data is
-    #  ancestor of other, then the ancestor key becomes LCA
-    if root.data == n1 or root.data == n2:
-        return root
-
-    # Look for datas in left and right subtrees
-    left_lca = findLCA(root.left, n1, n2)
-    right_lca = findLCA(root.right, n1, n2)
-
-    # If both of the above calls return Non-NULL, then one data
-    # is present in once subtree and other is present in other,
-    # So this node is the LCA
-    if left_lca and right_lca:
-        return root
-
-    # Otherwise check if left subtree or right subtree is LCA
-    return left_lca if left_lca is not None else right_lca
-
-
-root = newNode(3)
-root.left = newNode(5)
-root.right = newNode(1)
-root.left.left = newNode(6)
-root.left.right = newNode(2)
-root.right.left = newNode(0)
-root.right.right = newNode(8)
-root.left.right.left = newNode(7)
-root.left.right.right = newNode(4)
-
-
-print(findLCA(root, 5, 4).data)
+# print(findLCA(root, 5,4).data)
 
 
 # elelemt:
+# setup parent: https://towardsdatascience.com/binary-tree-and-lowest-common-ancestor-58eddd433ac
 # The algorithm is structually similar to a recursive postorder traveral, and the complexities are the same. Specifically, the time complexity and space complexity are O(n) and O(h), respectively, where h is the height of the tree.
 
+import collections
+from collections import deque
 
-class BinaryTreeNode:
-    def __init__(self, data=None, left=None, right=None, parent=None):
-        self.data = data
+
+class Node:
+    def __init__(self, value, parent=None, left=None, right=None):
+        self.value = value
+        self.parent = parent
         self.left = left
         self.right = right
-        self.parent = parent
+
+    def add_to_dict(self, node_dict):
+
+        node_dict[self.value] = self
+
+
+def create_bt(value_queue):
+    """create binary tree"""
+
+    if len(value_queue) <= 0:
+        return None, None
+
+    node_dict = {}
+
+    root = Node(value_queue.popleft())
+
+    root.add_to_dict(node_dict)
+
+    current_queue = deque()
+
+    current_queue.append(root)
+
+    while len(current_queue) > 0 and len(value_queue) > 0:
+
+        current_node = current_queue.popleft()
+
+        left = value_queue.popleft()
+        if left is not None:
+            current_node.left = Node(left, parent=current_node)
+            current_node.left.add_to_dict(node_dict)
+            current_queue.append(current_node.left)
+
+        right = value_queue.popleft()
+        if right is not None:
+            current_node.right = Node(right, parent=current_node)
+            current_node.right.add_to_dict(node_dict)
+            current_queue.append(current_node.right)
+
+    return root, node_dict
+
+
+def create_bt_fls(value_list):
+    """create binary create from list"""
+
+    return create_bt(deque(value_list))
+
+# def lca(node1, node2):
+
+#     parents = set()
+
+#     cur_parent = node1
+#     while cur_parent is not None:
+#         parents.add(cur_parent)
+#         cur_parent = cur_parent.parent
+
+#     cur_parent = node2
+#     while cur_parent is not None:
+#         if cur_parent in parents:
+#             return cur_parent
+#         cur_parent = cur_parent.parent
+
+#     return None
 
 
 def lca(tree, node0, node1):
@@ -126,15 +185,7 @@ def lca(tree, node0, node1):
     return lca_helper(tree, node0, node1).ancestor
 
 
-root = BinaryTreeNode(3)
-root.left = BinaryTreeNode(5)
-root.right = BinaryTreeNode(1)
-root.left.left = BinaryTreeNode(6)
-root.left.right = BinaryTreeNode(2)
-root.right.left = BinaryTreeNode(0)
-root.right.right = BinaryTreeNode(8)
-root.left.right.left = BinaryTreeNode(7)
-root.left.right.right = BinaryTreeNode(4)
-
-
-print(lca(root, root.left, root.left.right.right).data)
+ls1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, None, None, None, None, 9, 10]
+bt1, node_dict = create_bt_fls(ls1)
+lca1 = lca(node_dict[0], node_dict[9], node_dict[10])
+print(lca1.value, "&&&")
