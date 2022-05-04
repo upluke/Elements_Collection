e27b421e8cb6ba46253571e68dc087993786e03b
upluke <lukeforadream@gmail.com>
2022-05-03 19:37:29 -0700

    roman to integer lc

--- strings/roman_to_integer.py
+++ strings/roman_to_integer.py
@@ -11,18 +11,22 @@ def roman_to_integer(s):
 print(roman_to_integer('MCMXCIV'))
 
 # lc:
-# def romanToInt(s):
-#   dic={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-#   prev=0
-#   curr=0
-#   total=0
 
-#   for i in range(len(s)):
-#     curr=dic[s[i]]
-#     if curr > prev:
-#       total=total+curr-2*prev
-#     else:
-#       total+=curr
-#     prev=curr
-#   return total
-# print(romanToInt('LVIII'))
+
+def romanToInt(s):
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    prev = 0
+    curr = 0
+    total = 0
+
+    for i in range(len(s)):
+        curr = dic[s[i]]
+        if curr > prev:
+            total = total+curr-2*prev
+        else:
+            total += curr
+        prev = curr
+    return total
+
+
+print(romanToInt('LVIII'))
